b4e20909903d0d0eacc40fec14a67b96919213ea
Sigurd Fosseng <sigurd@fosseng.net>
2013-04-18 10:15:14 +0200

    loadKart

--- web/js/kart.js
+++ web/js/kart.js
@@ -1,2 +1,15 @@
 (function() {
+	var loadKart = function(asId, toElement){
+		d3.xml("resources/norway.svg", "image/svg+xml", function(xml){
+			var img = $(xml.documentElement);
+			img.attr("id", asId);
+
+			img.attr("viewBox", "0 0 2104 2980");
+			img.attr("preserveAspectRatio", "xMinYMin meet");
+			img.attr("width","500");
+			img.attr("height","700");
+			$(toElement).append(img)
+		});
+	};
+	loadKart("kart", "body");
 })();
